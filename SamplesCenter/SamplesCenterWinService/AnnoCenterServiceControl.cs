1ed899a989781a7387afc94a1a5fc2820ed6f867
duyanming <dym880@163.com>
2021-06-11 10:15:51 +0800

    增加 AnnoCenterWinService 示例

---
+++ SamplesCenter/SamplesCenterWinService/AnnoCenterServiceControl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Anno.Rpc.Center;
+using Topshelf;
+
+namespace SamplesCenterWinService
+{
+    class AnnoCenterServiceControl : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            AnnoCenterStart(Program.Args);
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            if (Monitor.State)
+            {
+                Monitor.Stop();
+            }
+            return true;
+        }
+
+        private Task AnnoCenterStart(string[] args)
+        {
+            return Task.Factory.StartNew(() =>
+             {
+                 Bootstrap.StartUp(args
+                     , (service, noticeType) =>//上线下线
+                     {
+
+                     }, (newService, oldService) =>//服务配置更改
+                     {
+
+                     });
+             }, TaskCreationOptions.LongRunning);
+        }
+    }
+}
---
+++ SamplesCenter/SamplesCenterWinService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Anno.Rpc.Center;
+using Topshelf;
+
+namespace SamplesCenterWinService
+{
+    public class Program
+    {
+        public static string[] Args;
+
+        /// <summary>
+        /// Windows服务 Anno注册中心
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Program.Args = args;
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<AnnoCenterServiceControl>();
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Anno 注册中心");
+                x.SetDisplayName("Anno Center Service");
+                x.SetServiceName("AnnoCenterService");
+
+                x.StartAutomatically();
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
