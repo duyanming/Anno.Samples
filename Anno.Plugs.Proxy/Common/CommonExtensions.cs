17c05b33fd206d0f0d83d07a3796652a717b03a3
曹阳 <cy941022@vip.qq.com>
2022-03-28 17:03:37 +0800

    添加数据转换扩展

---
+++ Anno.Plugs.Proxy/Common/CommonExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anno.Plugs.Proxy.Common
+{
+    public static class CommonExtensions
+    {
+
+        /// <summary>
+        /// 将object转换为string类型信息。
+        /// </summary>
+        /// <param name="o">object。</param>
+        /// <param name="t">默认值。</param>
+        /// <returns>string。</returns>
+        public static string ToString(this object o, string t)
+        {
+            string info = string.Empty;
+            if (o == null)
+            {
+                info = t;
+            }
+            else
+            {
+                info = o.ToString();
+            }
+            return info;
+        }
+
+        /// <summary>
+        /// 将object转换为float类型信息。
+        /// </summary>
+        /// <param name="o">object。</param>
+        /// <param name="t">默认值。</param>
+        /// <returns>float。</returns>
+        public static float ToFloat(this object o, float t = default(float))
+        {
+            float info;
+            if (!float.TryParse(o.ToString(string.Empty), out info))
+            {
+                info = t;
+            }
+            return info;
+        }
+
+        /// <summary>
+        /// 转换成Int/Int32
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="round">是否四舍五入，默认false</param>
+        /// <returns></returns>
+        public static int ToInt(this object s, bool round = false)
+        {
+            if (s == null || s == DBNull.Value)
+                return 0;
+
+            if (s.GetType().IsEnum)
+            {
+                return (int)s;
+            }
+
+            if (s is bool b)
+                return b ? 1 : 0;
+
+            if (int.TryParse(s.ToString(), out int result))
+                return result;
+
+            var f = s.ToFloat();
+            return round ? Convert.ToInt32(f) : (int)f;
+        }
+    }
+}
